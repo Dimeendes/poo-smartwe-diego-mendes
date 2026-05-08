d5740d0963b93e8a9e46c852cda55f74e5a60a81
dimeendes <diegomendesantonio@gmail.com>
2026-05-08 08:59:14 -0300

    feat: aula3 - aplica encapsulamento com modificadores de acesso

--- src/br/com/smartwe/main/SistemaPrincipal.java
+++ src/br/com/smartwe/main/SistemaPrincipal.java
@@ -4,37 +4,39 @@ import br.com.smartwe.model.Ventilador;
 
 public class SistemaPrincipal {
     public static void main(String[] args) {
+        
+        System.out.println("\n--- Sistema SmartWe ---\n");
+       
         //Criando o ventilador 1 (Objeto 1)
         Ventilador ventilador1 = new Ventilador("Ventilador de mesa", 40.0, 10);
-        System.out.println("Temporizador Ventilador 1: ");
+        System.out.println("Velocidade Ventilador 1: ");
+        ventilador1.alterarVelocidade(2);
+        System.out.println("\nTemporizador Ventilador 1: ");
         ventilador1.acionarTemporizador(10);
-        ventilador1.definirVelocidade(2);
+        
         
         //Criando o ventilador 2 (Objeto 2)
         Ventilador ventilador2 = new Ventilador("Ventilador de teto", 60.0, 15);
-        System.out.println("Temporizador Ventilador 2: ");
-        ventilador2.acionarTemporizador(15);
-        ventilador2.definirVelocidade(3);
+        System.out.println("\nVelocidade Ventilador 2: ");
+        ventilador2.alterarVelocidade(0);
+        System.out.println("\nTemporizador Ventilador 2: ");
+        ventilador2.acionarTemporizador(15);//Ventilador 2 está desligado (velocidade 0), portanto não aciona o temporizador
+       
+        System.out.println("\n---Verificando status dos aparelhos: ---\n");//potenciaWh define o gasto energético ATUAl(portanto se desligado, o gasto é 0)
         
-        System.out.println("\n--- Sistema SmartWe ---\n");
-        
-        System.out.println("Modelo ventilador 1: " + ventilador1.modelo + " | Potencia: " + ventilador1.potenciaWh + "Wh | Tamanho: " + ventilador1.tamanhoCm + "cm | Funcao: " + ventilador1.funcao);
-        System.out.println("Modelo ventilador 2: " + ventilador2.modelo + " | Potencia: " + ventilador2.potenciaWh + "Wh | Tamanho: " + ventilador2.tamanhoCm + "cm | Funcao: " + ventilador2.funcao + "\n");
+        System.out.println("Modelo ventilador 1: " + ventilador1.getModelo() + " | Potencia: " + ventilador1.getPotenciaWh() + "Wh | Tamanho: " + ventilador1.getTamanhoCm() + "cm | Funcao: " + ventilador1.getFuncao());
+        System.out.println("Modelo ventilador 2: " + ventilador2.getModelo() + " | Potencia: " + ventilador2.getPotenciaWh() + "Wh | Tamanho: " + ventilador2.getTamanhoCm() + "cm | Funcao: " + ventilador2.getFuncao() + "\n");
         
         System.out.println("Verificando velocidade: \n");
-        System.out.println("Velocidade Ventilador 1: " + ventilador1.velocidade);
-        System.out.println("Velocidade Ventilador 2: " + ventilador2.velocidade);
+        System.out.println("Velocidade Ventilador 1: " + ventilador1.getVelocidade());
+        System.out.println("Velocidade Ventilador 2: " + ventilador2.getVelocidade());
         
         System.out.println("\nVerificando temporizadores: \n");
-        System.out.println("Temporizador Ventilador 1: " + ventilador1.temporizadorMinutos + " minutos");
-        System.out.println("Temporizador Ventilador 2: " + ventilador2.temporizadorMinutos + " minutos");
+        System.out.println("Temporizador Ventilador 1: " + ventilador1.getTemporizadorMinutos() + " minutos");
+        System.out.println("Temporizador Ventilador 2: " + ventilador2.getTemporizadorMinutos() + " minutos");
 
-        //Testes com valores inválidos
-        /*System.out.println("\nTestes com valores inválidos: \n");
-        ventilador1.acionarTemporizador(-1);
-        ventilador1.definirVelocidade(-1);
-        ventilador2.acionarTemporizador(-1);
-        ventilador2.definirVelocidade(4);
-        */
+        //Testes para "hackear" sistema
+        /*ventilador1.potenciaWh = 100;
+        ventilador2.velocidade = 2;*/
     }
 }
--- src/br/com/smartwe/model/Ventilador.java
+++ src/br/com/smartwe/model/Ventilador.java
@@ -1,37 +1,97 @@
 package br.com.smartwe.model;
 
 public class Ventilador {
-    public String modelo;
-    public double potenciaWh;
-    public int tamanhoCm;
-    public String funcao;
-    public int temporizadorMinutos; //Atributo novo para o método acionarTemporizador
-    public int velocidade; //Atributo novo para o método definirVelocidade
+    private String modelo;
+    private double potenciaWh;
+    private int tamanhoCm;
+    private String funcao;
+    private int temporizadorMinutos; //Atributo novo para o método acionarTemporizador
+    private int velocidade; //Atributo novo para o método definirVelocidade
 
     public Ventilador(String modelo, double potenciaWh, int tamanhoCm) {
-        this.modelo = modelo;
-        this.potenciaWh = potenciaWh;
-        this.tamanhoCm = tamanhoCm;
-        this.funcao = "Ventilacao";
-        this.temporizadorMinutos = 0;
-        this.velocidade = 0;
+        this.setModelo(modelo);
+        this.setPotenciaWh(potenciaWh);
+        this.setTamanhoCm(tamanhoCm);
+        this.setFuncao("Ventilacao");
+        this.setTemporizadorMinutos(0);
+        this.setVelocidade(0);
+    }
+    
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public double getPotenciaWh() {
+        return this.potenciaWh;
+    }
+
+    private void setPotenciaWh(double potenciaWh) {
+        if(potenciaWh >= 0) {
+            this.potenciaWh = potenciaWh;
+        } else {
+            System.out.println("A potencia deve ser maior que 0");
+        }
+    }
+
+    public int getTamanhoCm() {
+        return this.tamanhoCm;
+    }
+
+    public String getFuncao() {
+        return this.funcao;
+    }
+
+    public int getTemporizadorMinutos() {
+        return this.temporizadorMinutos;
+    }
+
+    private void setTemporizadorMinutos(int temporizadorMinutos) {
+        if(temporizadorMinutos >= 0) {
+            this.temporizadorMinutos = temporizadorMinutos;
+        }else{
+            System.out.println("O temporizador deve ser maior que 0 minutos");
+        }
+    }
+
+    public int getVelocidade() {
+        return this.velocidade;
+    }
+
+    private void setVelocidade(int velocidade) {
+        if(velocidade >= 0 && velocidade <= 3) {
+            this.velocidade = velocidade;
+        }else{
+            System.out.println("A velocidade deve ser positiva e menor que 3");
+        }
     }
 
     public void acionarTemporizador(int minutos) {
-        if(minutos <= 0) {
-            System.out.println("O temporizador deve ser maior que 0 minutos");
-            return;
+        if(getVelocidade() != 0){
+            if(minutos <= 0) {
+                System.out.println("O temporizador deve ser maior que 0 minutos");
+            }
+            this.temporizadorMinutos = minutos;
+            System.out.println("Temporizador acionado para " + minutos + " minutos");
+        }else{
+            System.out.println("O ventilador deve ser ligado para acionar o temporizador");
         }
-        this.temporizadorMinutos = minutos;
-        System.out.println("Temporizador acionado para " + minutos + " minutos");
     }
     
-    public void definirVelocidade(int velocidade) {
+    public void alterarVelocidade(int velocidade) { //Por padronizar, velocidade 1 seria gasto padrão
         if(velocidade < 0 || velocidade > 3) {
             System.out.println("A velocidade deve ser positiva e menor que 3");
-            return;
+
+        }else{
+            if(velocidade == 0){
+                System.out.println("O ventilador foi desligado");
+                setPotenciaWh(0);
+            }else if(velocidade == 2){
+                setPotenciaWh(getPotenciaWh() * 1.1); //Velocidade 2 é 10% maior que a velocidade 1
+            }else if(velocidade == 3){
+                setPotenciaWh(getPotenciaWh() * 1.25); //Velocidade 3 é 25% maior que a velocidade 1
+            }
         }
-        this.velocidade = velocidade;
+        setVelocidade(velocidade);
         System.out.println("Velocidade definida para " + velocidade);
     }
 }
